9dbfa849e2e70941feedd60e697433856a2a3451
Dvin <dvyu@yandex.ru>
2020-10-23 21:28:44 +0500

    run

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,3 +7,5 @@ echo "$KUBE_CONFIG_DATA" | base64 --decode > /tmp/config
 export KUBECONFIG=/tmp/config
 kubectl config current-context
 echo YAML_FILE=${INPUT_YAML_FILE}
+echo BUILD_NUMBER_PREFIX=${BUILD_NUMBER_PREFIX}
+echo BUILD_NUMBER=${BUILD_NUMBER}
