0f4a79e11b029224d913040e155393a2e202aee8
Dvin <dvyu@yandex.ru>
2020-10-24 17:09:16 +0500

    add git user

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,8 @@ echo YAML_FILE=${YAML_FILE}
 grep -i image: ${YAML_FILE}
 echo BUILD_NUMBER_PREFIX=${BUILD_NUMBER_PREFIX}
 echo BUILD_NUMBER=${BUILD_NUMBER}
+TAG="${BUILD_NUMBER_PREFIX}.${BUILD_NUMBER}"
+echo TAG=${TAG}
 DOCKER_IMAGE=$(grep image: ${YAML_FILE} | awk -F: '{printf("%s\n",$2)}' | awk '{print $1}')
 DOCKER_IMAGE_SLASH=$(echo ${DOCKER_IMAGE} | sed 's#/#\\/#g')
 echo DOCKER_IMAGE_SLASH=${DOCKER_IMAGE_SLASH}
@@ -17,6 +19,8 @@ sed -E "s/image: .+$/image: ${DOCKER_IMAGE_SLASH}:${TAG}/" ${YAML_FILE} > ${YAML
 mv ${YAML_FILE}.tmp ${YAML_FILE}
 grep -i image: ${YAML_FILE}
 cat .git/config
+git config --global user.name "GIT_USER"
+git config --global user.email "GIT_EMAIL"
 git log -3
 git add -A
 git diff --cached
