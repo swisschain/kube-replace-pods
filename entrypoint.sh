2e695588ac36ca5c1bc56bd43049afa2dcddadb3
Sergey Korolev <32951007+1scrooge@users.noreply.github.com>
2021-07-21 16:35:17 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Extract the base64 encoded config data and write this to the KUBECONFIG
-echo "$KUBE_CONFIG_DATA" | base64 --decode > /tmp/config
+echo "$KUBE_CONFIG_DATA" | base64 -d > /tmp/config
 export KUBECONFIG=/tmp/config
 kubectl config current-context
 echo YAML_FILE=${YAML_FILE}
